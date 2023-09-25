2dfb8d59bcb05629bb6a9ef52e08237535fd4441
Deobap73 <deolindoalberto.dasilvabaptista>
2023-09-25 20:28:03 +0200

    resetting pins on the map

--- src/Components/WorldMap.jsx
+++ src/Components/WorldMap.jsx
@@ -45,11 +45,11 @@ function WorldMap({
         <button className={styles.pragh} onClick={openCityContainer3}>
           <img className={styles.vectorIcon} alt="" src="/vector3@2x.png" />
         </button>
-        <button className={styles.sal} onClick={openCityContainer4}>
-          <img className={styles.vectorIcon4} alt="" src="/vector4@2x.png" />
+        <button className={styles.salo} onClick={openCityContainer4}>
+          <img className={styles.vectorIcon} alt="" src="/vector4@2x.png" />
         </button>
         <button className={styles.venice} onClick={openCityContainer5}>
-          <img className={styles.vectorIcon5} alt="" src="/vector5@2x.png" />
+          <img className={styles.vectorIcon} alt="" src="/vector5@2x.png" />
         </button>
         <button className={styles.newyork} onClick={openCityContainer6}>
           <img className={styles.vectorIcon} alt="" src="/vector6@2x.png" />
@@ -61,34 +61,34 @@ function WorldMap({
           <img className={styles.vectorIcon} alt="" src="/vector8@2x.png" />
         </button>
         <button className={styles.tarifa} onClick={openCityContainer9}>
-          <img className={styles.vectorIcon9} alt="" src="/vector9@2x.png" />
+          <img className={styles.vectorIcon} alt="" src="/vector9@2x.png" />
         </button>
         <button className={styles.helgoland} onClick={openCityContainer10}>
-          <img className={styles.vectorIcon10} alt="" src="/vector10@2x.png" />
+          <img className={styles.vectorIcon} alt="" src="/vector10@2x.png" />
         </button>
         <button className={styles.rome} onClick={openCityContainer11}>
-          <img className={styles.vectorIcon11} alt="" src="/vector11@2x.png" />
+          <img className={styles.vectorIcon} alt="" src="/vector5@2x.png" />
         </button>
         <button className={styles.cuzco} onClick={openCityContainer12}>
           <img className={styles.vectorIcon} alt="" src="/vector12@2x.png" />
         </button>
         <button className={styles.quebec} onClick={openCityContainer13}>
-          <img className={styles.vectorIcon13} alt="" src="/vector13@2x.png" />
+          <img className={styles.vectorIcon} alt="" src="/vector13@2x.png" />
         </button>
         <button className={styles.paris} onClick={openCityContainer14}>
-          <img className={styles.vectorIcon14} alt="" src="/vector14@2x.png" />
+          <img className={styles.vectorIcon} alt="" src="/vector14@2x.png" />
         </button>
         <button className={styles.mexicocity} onClick={openCityContainer15}>
           <img className={styles.vectorIcon} alt="" src="/vector15@2x.png" />
         </button>
         <button className={styles.herat} onClick={openCityContainer16}>
-          <img className={styles.vectorIcon16} alt="" src="/vector16@2x.png" />
+          <img className={styles.vectorIcon} alt="" src="/vector16@2x.png" />
         </button>
         <button className={styles.dubai} onClick={openCityContainer17}>
-          <img className={styles.vectorIcon17} alt="" src="/vector17@2x.png" />
+          <img className={styles.vectorIcon} alt="" src="/vector17@2x.png" />
         </button>
         <button className={styles.amsterdam} onClick={openCityContainer18}>
-          <img className={styles.vectorIcon18} alt="" src="/vector18@2x.png" />
+          <img className={styles.vectorIcon} alt="" src="/vector18@2x.png" />
         </button>
         <button className={styles.kotor} onClick={openCityContainer19}>
           <img className={styles.vectorIcon} alt="" src="/vector19@2x.png" />
