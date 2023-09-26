015b39494bb9aad5be3f7b8d82d4a69bf5fcebef
Deobap73 <deolindoalberto.dasilvabaptista>
2023-09-26 10:26:44 +0200

    test

--- src/Components/EssenceOfTraveling.jsx
+++ src/Components/EssenceOfTraveling.jsx
@@ -1,14 +1,35 @@
-import './EssenceOfTraveling.css'; // Import your styles
+
+import './EssenceOfTraveling.css';
 import EssenceOfTravelingLeft from "./EssenceOfTraveling/EssenceOfTravelingLeft";
 import EssenceOfTravelingRight from "./EssenceOfTraveling/EssenceOfTravelingRight";
+import data from "../api/projectTravel.json";
 
 function EssenceOfTraveling() {
   return (
-    <div className="essenceoftraveling">
-      <EssenceOfTravelingLeft />
-      <EssenceOfTravelingRight />
-      
+    <><div className="essenceoftraveling">
+      {data.data.map((item) => (
+        <div key={item.id}>
+          {item.id.includes("Left") ? (
+            <EssenceOfTravelingLeft
+              image={item.Image}
+              statement={item.Statement}
+              title={item.Title}
+              sentences={item.Sentences}
+            />
+          ) : (
+            <EssenceOfTravelingRight
+              image={item.Image}
+              statement={item.Statement}
+              title={item.Title}
+              sentences={item.Sentences}
+            />
+          )}
+        </div>
+      ))}
     </div>
+      <h2></h2>
+    </>
+    
   );
 }
 
