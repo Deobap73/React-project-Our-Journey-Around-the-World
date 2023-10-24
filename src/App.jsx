302801040509ddfe89518c4eaaf69a8a81bd3bcf
Deobap73 <deolindoalberto.dasilvabaptista>
2023-10-24 16:11:00 +0200

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Footer from "./Components/Footer";
 import NavigationBar from "./Components/NavigationBar";
 import Home from "./Pages/Home";
